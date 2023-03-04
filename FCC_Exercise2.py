52f7403548ad22c29dc4b27d32b6bad05c0c473c
JoseMamuel <126986733+JoseMamuel@users.noreply.github.com>
2023-03-04 17:46:24 -0600

    Add files via upload

---
+++ FCC_Exercise2.py
@@ -0,0 +1,7 @@
+def main():
+    hour = input("Enter Hours : ")
+    rate = input("Enter Rate $ ")
+    pay = float(hour)*float(rate)
+    print("Pay : $",pay)
+if __name__ == "__main__":
+    main()   
